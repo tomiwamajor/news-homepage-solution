34c6a1bb766d72b6e293fb1bd62ce3462aaf7f52
Adelekun Adetomiwa Kolapo <adetomiwaadelekun@gmail.com>
2023-10-11 17:19:49 +0100

    changes

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const open = document.querySelector(".hidden");
 const close = document.querySelector("hidden2");
+const menu = document.querySelector(".menu-links");
 
-open.addEventListener("click", function () {
-  console.log("You clicked it");
-  document.querySelector(".hidden2").style.display = "block";
-  document.querySelector(".hidden").style.display = "none";
-  document.querySelector(".menu-link").style.visibility = "display";
+open.addEventListener("click", () => {
+  // console.log("You clicked it");
+  // document.querySelector(".hidden2").style.display = "block";
+  // document.querySelector(".hidden").style.display = "none";
+  // document.querySelector(".menu-link").style.visibility = "display";
+  menu.classList.toggle("menu-open");
 });
